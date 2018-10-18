abf95536e28be92c3d13684c6f548204e3715e86
tim-hub <tim_w@outlook.com>
2018-10-18 15:12:25 +1300

    add dq log sql

---
+++ Create_DQ_Log.sql
@@ -0,0 +1,12 @@
+if exists (select * from sys.tables where name='DQLog')
+	drop table DQLog;
+go
+
+CREATE TABLE DQLog(
+	LogID int Primary KEY IDENTITY(1,1),
+	RowID BINARY(8),
+	DBName nvarchar(20) NOT NULL,
+	TableName nvarchar(20) NOT NULL,
+	RuleNo tinyint not null check (RuleNo >= 1 and RuleNo <=52),
+	Action nvarchar(6) not null  check (Action in ('Allow', 'Fix', 'Reject'))
+)
